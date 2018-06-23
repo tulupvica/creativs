f639d496601e11eca2fff18df37c5953353303fa
unknown <viktoriatulup@gmail.com>
2018-06-23 14:50:31 +0300

    added phone-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,13 @@
-console.log( 'Hi' );
+$( document ).ready(function() {
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        $(this).toggleClass('menu-btn__active')
+    });
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        $('.phone-menu').toggleClass('phone-menu__active')
+    });
+
+});
